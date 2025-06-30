081bff47eace5043152acd0f594ef8fa88b95fd2
PiLB <pierbacq@gmail.com>
2025-06-30 15:49:00 +0200

    task services

---
+++ src/tasks/task.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Task } from './task.interface';
+
+@Injectable
+export class TaskService {
+    private taskList:Task[] = [];
+    private taskNumber:number = 0;
+
+    getAll():Task[]{
+        return this.taskList;
+    }
+
+    getByID(id:number){
+        const task=this.taskList.find(t => t.id === id);
+        if (!task){
+            throw new NotFoundException('No task found with number ${id}');
+        }
+        return task;
+    }
+
+    markAsDone(id:number): Task {
+        const task=this.getByID(id);
+        task.done=!task.done;
+        return task;
+    }
+
+    delete(id:number): void{
+        const index = this.taskList.findIndex(t => t.id === id);
+        if (index === -1){
+            throw new NotFoundException('Task with ID ${id} not found.');
+        }
+        this.taskList.splice(index,1);
+    }
+
+    newTask(title:string,urgency?:number): Task {
+        const newTask: Task = {
+            id: this.taskNumber++,
+            title,
+            done: false,
+            urgency
+        };
+        this.taskList.push(newTask);
+        return newTask;
+    }
+
+
+
+
+
+}
